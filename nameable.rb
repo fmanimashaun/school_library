ca3d22b7495a920207e27c04295fc18d41665796
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-09 12:53:30 +0100

    Create the nameanle class

---
+++ nameable.rb
@@ -0,0 +1,6 @@
+# the is a module that can be included in any class that needs a name
+class Nameable
+  def correct_name
+    raise NotImplementedError, 'Subclasses must implement correct_name method'
+  end
+end
