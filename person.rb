1688ea7fbacd029767a7fe689018e809c3bb3020
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-10 23:23:25 +0100

    update person class

--- person.rb
+++ person.rb
@@ -6,8 +6,8 @@ require './rental'
 # The name is the name of the person.
 # The age is the age of the person.
 class Person < Nameable
-  attr_reader :id, :rentals
-  attr_accessor :name, :age
+  attr_reader :id,
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
