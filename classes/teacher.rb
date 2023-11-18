cca670aeaaaa7f594c3ee9645c374787e706eee9
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-18 08:37:39 +0100

    update the teacher class
    Co-authored-by: Ademola Owoeye <ademolaowoeye07@gmail.com>

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -1,8 +1,9 @@
 require_relative 'person'
+require 'json'
 
 class Teacher < Person
-  def initialize(age, name, specialization)
-    super(age, name)
+  def initialize(age, name, specialization, id: Random.rand(1..1000))
+    super(age, name, id: id)
     @specialization = specialization
   end
 
@@ -10,7 +11,18 @@ class Teacher < Person
     true
   end
 
-  def to_hash
-    super.merge('specialization' => @specialization)
+  def to_json
+    {
+      'json_class' => self.class.name,
+      'id' => @id,
+      'name' => @name,
+      'age' => @age,
+      'parent_permission' => @parent_permission,
+      'specialization' => @specialization
+    }.to_json
+  end
+
+  def self.json_create(object)
+    new(object['age'], object['name'], parent_permission: object['parent_permission'], specialization: object['specialization'], id: object['id'])
   end
 end
