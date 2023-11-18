beea30c26a6ee6f39f52b20cac11205a8aeaff3e
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-18 09:02:33 +0100

    Update the teacher class
    Co-authored-by: Ademola Owoeye <ademolaowoeye07@gmail.com>

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 require 'json'
 
 class Teacher < Person
-  def initialize(age, name, specialization, id: Random.rand(1..1000))
+  def initialize(age, name = 'unknown', specialization, id: Random.rand(1..1000))
     super(age, name, id: id)
     @specialization = specialization
   end
@@ -11,19 +11,17 @@ class Teacher < Person
     true
   end
 
-  def to_json(*_args)
+  def to_json
     {
       'json_class' => self.class.name,
       'id' => @id,
       'name' => @name,
       'age' => @age,
-      'parent_permission' => @parent_permission,
       'specialization' => @specialization
     }.to_json
   end
 
   def self.json_create(object)
-    new(object['age'], object['name'], parent_permission: object['parent_permission'],
-                                       specialization: object['specialization'], id: object['id'])
+    new(object['age'], object['name'], object['specialization'], id: object['id'])
   end
 end
