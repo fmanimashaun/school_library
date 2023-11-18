0070739b9a17acc3a2eed8a9c1a28233f7cd0a1e
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-18 09:18:36 +0100

    Update the book class
    Co-authored-by: Ademola Owoeye <ademolaowoeye07@gmail.com>

--- classes/book.rb
+++ classes/book.rb
@@ -1,4 +1,5 @@
 require_relative 'rental'
+require 'json'
 
 class Book
   attr_accessor :title, :author, :rentals
@@ -13,10 +14,15 @@ class Book
     Rental.new(date, self, person)
   end
 
-  def to_hash
+  def to_json
     {
+      'json_class' => self.class.name,
       'title' => @title,
       'author' => @author
-    }
+  }.to_json
+  end
+
+  def self.json_create(object)
+    new(object['title'], object['author'])
   end
 end
